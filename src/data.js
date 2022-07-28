0a2f57c5568a02271af558133153e510c78ff64b
Aydıncan Kalyoncu <aydincankalyncu@gmail.com>
2022-07-28 23:57:06 +0300

    React fronend with styled components

---
+++ src/data.js
@@ -0,0 +1,96 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img:"https://images6.alphacoders.com/637/thumb-1920-637352.jpg",
+        title: "JOHN WICK 1",
+        desc: "SEEMS LIKE YOU ARE GOING TO BUY IT",
+        bg: "fcf1ed"
+    },
+    {
+        id: 2,
+        img:"https://initiate.alphacoders.com/images/831/cropped-1920-1080-831125.jpg?2865",
+        title: "JOHN WICK 2",
+        desc: "SEEMS LIKE YOU ARE GOING TO BUY IT",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img:"https://initiate.alphacoders.com/images/831/cropped-1920-1080-831123.jpg?8851",
+        title: "JOHN WICK 3",
+        desc: "SEEMS LIKE YOU ARE GOING TO BUY IT",
+        bg: "fbf0f4"
+    },
+    {
+        id: 4,
+        img:"https://initiate.alphacoders.com/images/105/cropped-1920-1080-1051994.jpg?788",
+        title: "JOHN WICK 4 (COMING SOON)",
+        desc: "SEEMS LIKE YOU ARE GOING TO BUY IT",
+        bg: "fbf0f4"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img:"https://images6.alphacoders.com/637/thumb-1920-637352.jpg",
+        title: "JOHN WICK 1"
+    },
+    {
+        id: 2,
+        img:"https://initiate.alphacoders.com/images/831/cropped-1920-1080-831125.jpg?2865",
+        title: "JOHN WICK 2"
+    },
+    {
+        id: 3,
+        img:"https://initiate.alphacoders.com/images/831/cropped-1920-1080-831123.jpg?8851",
+        title: "JOHN WICK 3"
+    },
+    {
+        id: 4,
+        img:"https://initiate.alphacoders.com/images/105/cropped-1920-1080-1051994.jpg?788",
+        title: "JOHN WICK 4 (COMING SOON)"
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 1"
+    },
+    {
+        id: 2,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 2"
+    },
+    {
+        id: 3,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 3"
+    },
+    {
+        id: 4,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 4 (COMING SOON)"
+    },
+    {
+        id: 5,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 4 (COMING SOON)"
+    },
+    {
+        id: 6,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 4 (COMING SOON)"
+    },
+    {
+        id: 7,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 4 (COMING SOON)"
+    },
+    {
+        id: 8,
+        img:"https://apprecs.org/ios/images/app-icons/256/a6/1271309990.jpg",
+        title: "JOHN WICK 4 (COMING SOON)"
+    }
+]
