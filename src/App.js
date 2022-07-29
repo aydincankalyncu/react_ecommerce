519e9ad5157a1f1131f8c13ff92989d12e26f5dd
Aydıncan Kalyoncu <aydincankalyncu@gmail.com>
2022-07-30 00:08:12 +0300

    New screens added.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Home from './pages/Home'
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 function App() {
-  return (
-    <Home></Home>
-  );
+  return <Product/>
 }
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,12 @@
+import {
+  Facebook,
+  Twitter,
+  Instagram,
+  LinkedIn,
+  Room,
+  Phone,
+  MailOutline,
+} from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -6,16 +15,122 @@ const Container = styled.div`
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
 `;
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
 `;
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
 `;
 
 const Footer = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Left>
+        <Logo>KALYONCU</Logo>
+        <Desc>Contact with us</Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook></Facebook>
+          </SocialIcon>
+          <SocialIcon color="E440FF">
+            <Instagram></Instagram>
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter></Twitter>
+          </SocialIcon>
+          <SocialIcon color="3B5999">
+            <LinkedIn></LinkedIn>
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Orders</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Home</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          {" "}
+          <Room style={{marginRight:"10px"}}/>
+          Ankara, Turkey
+        </ContactItem>
+        <ContactItem>
+          {" "}
+          <Phone style={{marginRight:"10px"}} /> +90 555 468 4875
+        </ContactItem>
+        <ContactItem>
+          {" "}
+          <MailOutline style={{marginRight:"10px"}} />
+          kalyoncu@kalync.com
+        </ContactItem>
+        <Payment src="https://images6.alphacoders.com/637/thumb-1920-637352.jpg" />
+      </Right>
+    </Container>
+  );
 };
 
 export default Footer;
