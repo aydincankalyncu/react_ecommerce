0405e9649a0e5ab05685e320cf0527e14e92a8ea
Aydıncan Kalyoncu <aydincankalyncu@gmail.com>
2022-08-10 00:14:22 +0300

    Login, register and cart pages added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 import { Badge } from "@material-ui/core";
+import {mobile} from '../responsive'
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({backgroundColor:"red"})}
 `;
 
 const Wrapper = styled.div`
